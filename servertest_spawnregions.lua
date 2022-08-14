2a33e1535daf80b35398409d0228494f57c5f301
Victor Balastegui <victorbalastegui@gmail.com>
2022-08-14 14:55:45 +0200

    default server conf

---
+++ servertest_spawnregions.lua
@@ -0,0 +1,11 @@
+-- This file was automatically generated when the server was first started.
+-- Clients connecting to the server can choose to spawn in one of the following spawn regions.
+-- The administrator can add as many different spawn regions as he/she wants.
+function SpawnRegions()
+	return {
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		-- Uncomment the line below to add a custom spawnpoint for this server.
+--		{ name = "Twiggy's Bar", serverfile = "servertest_spawnpoints.lua" },
+	}
+end
